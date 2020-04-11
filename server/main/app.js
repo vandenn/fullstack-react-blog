ae42d60c673fc04a2e332365710fef92f56c5c9e
Vandenn <evan.livelo@gmail.com>
2020-04-11 13:57:14 +0800

    feat: Add sample route to server

--- server/main/app.js
+++ server/main/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var indexRouter = require('./routes');
+
 var app = express();
 
 app.use(logger('dev'));
@@ -11,5 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
 
 module.exports = app;
