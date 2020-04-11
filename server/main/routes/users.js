1cefad44fb456338b298c589c9f582db23b42113
Vandenn <evan.livelo@gmail.com>
2020-04-11 21:12:12 +0800

    feat: Add server REST routes for users

--- server/main/routes/users.js
+++ server/main/routes/users.js
@@ -2,7 +2,50 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json('Users');
+  const username = String(req.query.username);
+  pool.query(
+    `SELECT * FROM users
+  WHERE username=$1`,
+    [username],
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+    }
+  );
+});
+
+router.get('/:id', (req, res) => {
+  pool.query(
+    `SELECT * FROM users
+  WHERE uid=$1`,
+    [req.params.id],
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+    }
+  );
+});
+
+router.get('/:id/posts', (req, res) => {
+  pool.query(
+    `SELECT * FROM posts
+  WHERE user_id=$1`,
+    [req.params.id],
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+    }
+  );
+});
+
+router.post('/', (req, res) => {
+  const { username, email, email_verified } = req.body;
+  pool.query(
+    `INSERT INTO users(username, email, email_verified, date_created)
+  VALUES ($1, $2, $3, NOW())
+  ON CONFLICT DO NOTHING`,
+    [username, email, email_verified],
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+    }
+  );
 });
 
 module.exports = router;
