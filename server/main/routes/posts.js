8597fcc57c39659d5a18c965a0fe633971fd2084
Vandenn <evan.livelo@gmail.com>
2020-04-11 16:17:44 +0800

    feat: Add initial REST routes for posts

--- server/main/routes/posts.js
+++ server/main/routes/posts.js
@@ -1,8 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var pool = require('../db');
 
 router.get('/', (req, res) => {
-  res.json('Posts');
+  pool.query(
+    `SELECT * FROM posts 
+    ORDER BY date_created DESC`,
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+    }
+  );
+});
+
+router.get('/:id', (req, res) => {
+  pool.query(
+    `SELECT * FROM posts WHERE pid=$1`,
+    [req.params.id],
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+    }
+  );
+});
+
+router.post('/', (req, res, next) => {
+  const { title, body, uid, username } = req.body;
+  pool.query(
+    `INSERT INTO posts(title, body, user_id, author, date_created) 
+  VALUES ($1, $2, $3, $4, NOW())`,
+    [title, body, uid, username],
+    (q_err, q_res) => {
+      if (q_err) return next(q_err);
+      res.json(q_res.rows);
+    }
+  );
 });
 
 module.exports = router;
