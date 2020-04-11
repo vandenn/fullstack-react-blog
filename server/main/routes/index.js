f417d914eff3e004d67daf28b93af17ae4c74dba
Vandenn <evan.livelo@gmail.com>
2020-04-11 19:16:17 +0800

    chore: Remove empty comments subroute file

--- server/main/routes/index.js
+++ server/main/routes/index.js
@@ -6,7 +6,6 @@ router.get('/hello', (req, res) => {
 });
 
 router.use('/posts', require('./posts'));
-router.use('/comments', require('./comments'));
 router.use('/users', require('./users'));
 
 module.exports = router;
