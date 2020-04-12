bea2cc856f7cb125d3f914566c29d6b6f2e33db4
Vandenn <evan.livelo@gmail.com>
2020-04-12 18:41:48 +0800

    feat: Setup redux reducers and sagas boilerplate

---
+++ client/src/Root.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import reducers from 'reducers';
+import rootSaga from 'sagas';
+
+export default ({ children, initialState = {} }) => {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const sagaMiddleware = createSagaMiddleware();
+  const store = createStore(
+    reducers,
+    initialState,
+    composeEnhancers(applyMiddleware(sagaMiddleware))
+  );
+  sagaMiddleware.run(rootSaga);
+
+  return <Provider store={store}>{children}</Provider>;
+};
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Root from './Root';
 import { Auth0Provider } from './contexts/auth0';
 
 ReactDOM.render(
-  <Auth0Provider>
-    <App />
-  </Auth0Provider>,
+  <Root>
+    <Auth0Provider>
+      <App />
+    </Auth0Provider>
+  </Root>,
   document.getElementById('root')
 );
