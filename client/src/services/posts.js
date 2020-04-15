2f3d4f5208fd7334bfe60b0ac460f5c232be5bb8
Vandenn <evan.livelo@gmail.com>
2020-04-16 02:28:57 +0800

    feat: Create add post service function

--- client/src/services/posts.js
+++ client/src/services/posts.js
@@ -4,3 +4,7 @@ import { postsBasePath } from '../constants/backendPaths';
 export const fetchAllPosts = () => {
   return axios.get(postsBasePath);
 };
+
+export const addPost = ({ title, body, uid }) => {
+  return axios.post(postsBasePath, { title, body, uid });
+};
