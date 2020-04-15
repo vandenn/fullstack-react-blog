b372ddcfd84b003e3d529f0dc73ca6f43834c453
Vandenn <evan.livelo@gmail.com>
2020-04-16 02:24:26 +0800

    chore: Remove duplicate fields in schema sql

--- server/main/schema.sql
+++ server/main/schema.sql
@@ -13,16 +13,13 @@ CREATE TABLE posts (
   title VARCHAR(255),
   body VARCHAR,
   user_id INT REFERENCES users(uid),
-  author VARCHAR REFERENCES users(username),
   date_created TIMESTAMP,
-  like_user_id INT[] DEFAULT ARRAY[]::INT[],
-  likes INT DEFAULT 0
+  like_user_id INT[] DEFAULT ARRAY[]::INT[]
 );
 
 CREATE TABLE comments (
   cid SERIAL PRIMARY KEY,
   comment VARCHAR(255),
-  author VARCHAR REFERENCES users(username),
   user_id INT REFERENCES users(uid),
   post_id INT REFERENCES posts(pid),
   date_created TIMESTAMP
