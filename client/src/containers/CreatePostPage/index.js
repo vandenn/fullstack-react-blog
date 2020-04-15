95b65387ee9f78ce3681db64f25ad1b36521e6d2
Vandenn <evan.livelo@gmail.com>
2020-04-16 02:58:10 +0800

    feat: Add form fields for create post page

--- client/src/containers/CreatePostPage/index.js
+++ client/src/containers/CreatePostPage/index.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { makeCurrentUserSelector } from '../../selectors/data/currentUser';
 
 const CreatePostPage = props => {
-  return <div>Create post page</div>;
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const currentUserSelector = useMemo(makeCurrentUserSelector, []);
+  const currentUser = useSelector(currentUserSelector);
+
+  const handleTitleChange = event => setTitle(event.target.value);
+  const handleBodyChange = event => setBody(event.target.value);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (!currentUser) return;
+    console.log(currentUser.uid);
+  };
+
+  return (
+    <div>
+      <h1>Compose</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input type='text' value={title} onChange={handleTitleChange} />
+        </label>
+        <br />
+        <label>
+          Body:
+          <textarea value={body} onChange={handleBodyChange} />
+        </label>
+        <input type='submit' value='Submit' />
+      </form>
+    </div>
+  );
 };
 
 export default CreatePostPage;
