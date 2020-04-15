50418e23de58cec9d159844273228ad61fcf603f
Vandenn <evan.livelo@gmail.com>
2020-04-16 02:34:00 +0800

    feat: Create add post page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import HomePage from './containers/HomePage';
+import AddPostPage from './containers/AddPostPage';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Router>
         <Switch>
           <Route exact path='/' component={HomePage} />
+          <Route exact path='/compose' component={AddPostPage} />
         </Switch>
       </Router>
     </>
---
+++ client/src/containers/AddPostPage/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddPostPage = props => {
+  return <div>Add post page</div>;
+};
+
+export default AddPostPage;
