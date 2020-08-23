1e05b3c49fba82c1beebf5722caae76c33e80714
Vandenn <evan.livelo@gmail.com>
2020-08-23 23:45:30 +0800

    refactor: Hide create post button when not logged in

--- client/src/containers/HomePage/index.js
+++ client/src/containers/HomePage/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import history from '_history';
 
+import { useAuth0 } from 'contexts/auth0';
 import * as routes from 'constants/frontendRoutes';
+import { makeCurrentUserSelector } from 'selectors/data/currentUser';
 
 const HomePage = (props) => {
+  const { isLoading } = useAuth0();
+  const currentUserSelector = useMemo(makeCurrentUserSelector, []);
+  const currentUser = useSelector(currentUserSelector);
+
   const handleCreatePostClick = (event) => {
     history.push(routes.createPost);
+  };
+
+  const renderCreatePostButton = () => {
+    if (!isLoading && !currentUser) {
+      return <button onClick={handleCreatePostClick}>Create Post</button>;
+    }
+    return null;
   };
 
   return (
     <div>
       <h1>Home Page</h1>
-      <button onClick={handleCreatePostClick}>Create Post</button>
+      {renderCreatePostButton()}
     </div>
   );
 };
