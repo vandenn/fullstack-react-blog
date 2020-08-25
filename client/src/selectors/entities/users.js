e8cbae32c351011bcd8c0a195db4196092fb2e4d
Vandenn <evan.livelo@gmail.com>
2020-08-25 23:30:23 +0800

    feat: Add additional users selectors

--- client/src/selectors/entities/users.js
+++ client/src/selectors/entities/users.js
@@ -4,3 +4,25 @@ import { entitiesSelector } from './';
 
 export const makeUsersSelector = () =>
   createSelector([entitiesSelector], (entities) => entities.users);
+
+export const makeUserSelectorById = () => {
+  const usersSelector = makeUsersSelector();
+  return createSelector(
+    [usersSelector, (_, props) => props.id],
+    (users, id) => users[id]
+  );
+};
+
+export const makeUsernameSelector = () => {
+  const userSelectorById = makeUserSelectorById();
+  return createSelector([userSelectorById], (user) =>
+    user ? user.username : ''
+  );
+};
+
+export const makeUserPictureSelector = () => {
+  const userSelectorById = makeUserSelectorById();
+  return createSelector([userSelectorById], (user) =>
+    user ? user.picture : ''
+  );
+};
