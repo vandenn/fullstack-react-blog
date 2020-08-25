6dcf5ecd2207aa084dce10b19917e2b8fb7deb8a
Vandenn <evan.livelo@gmail.com>
2020-08-25 15:37:11 +0800

    feat: Add selectors for getting specific post and its properties

--- client/src/selectors/entities/posts.js
+++ client/src/selectors/entities/posts.js
@@ -1,6 +1,33 @@
 import { createSelector } from 'reselect';
 
 import { entitiesSelector } from './';
+import { makeUsersSelector } from './users';
 
 export const makePostsSelector = () =>
   createSelector([entitiesSelector], (entities) => entities.posts);
+
+export const makePostSelectorById = () => {
+  const postsSelector = makePostsSelector();
+  return createSelector(
+    [postsSelector, (_, props) => props.id],
+    (posts, id) => posts[id]
+  );
+};
+
+export const makePostTitleSelector = () => {
+  const postSelectorById = makePostSelectorById();
+  return createSelector([postSelectorById], (post) => (post ? post.title : ''));
+};
+
+export const makePostBodySelector = () => {
+  const postSelectorById = makePostSelectorById();
+  return createSelector([postSelectorById], (post) => (post ? post.body : ''));
+};
+
+export const makePostAuthorSelector = () => {
+  const postSelectorById = makePostSelectorById();
+  const usersSelector = makeUsersSelector();
+  return createSelector([postSelectorById, usersSelector], (post, users) =>
+    post ? users[post.user_id] : ''
+  );
+};
