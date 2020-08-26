09378f96b067edf8474c9645ab0a3172025341be
Vandenn <evan.livelo@gmail.com>
2020-08-26 22:52:56 +0800

    feat: Add action for liking post

--- client/src/actions/requests/posts.js
+++ client/src/actions/requests/posts.js
@@ -4,6 +4,7 @@ export const types = {
   CREATE_POST: createActionTypeTriad('CREATE_POST'),
   FETCH_POST: createActionTypeTriad('FETCH_POST'),
   FETCH_RANGE_OF_POSTS: createActionTypeTriad('FETCH_RANGE_OF_POSTS'),
+  LIKE_POST: createActionTypeTriad('LIKE_POST');
 };
 
 export const actions = {
@@ -19,4 +20,8 @@ export const actions = {
     type: types.FETCH_RANGE_OF_POSTS.request,
     payload: { startIndex, endIndex },
   }),
+  likePost: (pid) => ({
+    type: types.LIKE_POST.request,
+    payload: { pid }
+  })
 };
