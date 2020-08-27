bf715efa23f079f80839bbaee1282417730c2308
Vandenn <evan.livelo@gmail.com>
2020-08-27 17:32:24 +0800

    feat: Add sagas for fetching and adding comments

--- client/src/sagas/requests/index.js
+++ client/src/sagas/requests/index.js
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
+import commentsRequestsSaga from './comments';
 import postsRequestsSaga from './posts';
 import usersRequestSaga from './users';
 
 export default function* rootSaga() {
-  yield all([postsRequestsSaga(), usersRequestSaga()]);
+  yield all([commentsRequestsSaga(), postsRequestsSaga(), usersRequestSaga()]);
 }
