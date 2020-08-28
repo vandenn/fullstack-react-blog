76a298d2774b9862876ea6388979f5dd927d0540
Vandenn <evan.livelo@gmail.com>
2020-08-28 20:45:53 +0800

    feat: Add action for getting total post count

---
+++ client/src/actions/data/posts.js
@@ -0,0 +1,11 @@
+import { createActionTypeTriad } from 'actions/utils';
+
+export const types = {
+  FETCH_TOTAL_POST_COUNT: createActionTypeTriad('FETCH_TOTAL_POST_COUNT'),
+};
+
+export const actions = {
+  fetchTotalPostCount: () => ({
+    type: types.FETCH_TOTAL_POST_COUNT,
+  }),
+};
